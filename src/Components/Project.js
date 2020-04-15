475c2667618385a7b53235ccbfb179f99b4f9f2d
tdonovan79 <tdonovan79@gmail.com>
2020-04-15 15:09:43 -0400

    spelling error in pokeon description

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -10,8 +10,8 @@ export default function Project(props) {
             <ul className='project-links-list'>
                 <li><p className="project-description">{desc}</p></li>
                 <li><a href={liveUrl} target="_blank" className='project-link'>Live Page</a></li>
-                <li><a href={gitFront} target="_blank" className='project-link'>Github Frontend</a></li>
-                <li><a href={gitBack} target="_blank" className='project-link'>Github Backend</a></li>
+                <li><a href={gitFront} target="_blank" className='project-link'>GitHub Frontend</a></li>
+                <li><a href={gitBack} target="_blank" className='project-link'>GitHub Backend</a></li>
                 <li><a href={demo} target="_blank" className='project-link'>Video Demo</a></li>
             </ul>
         </div>
--- src/Components/ProjectLinks.js
+++ src/Components/ProjectLinks.js
@@ -22,7 +22,7 @@ export default function ProjectLinks() {
             <Project
                 name='Fifteen Minute Food'
                 image={Scooter}
-                desc='Fifteen Minute Food is an app for users to order food. The front end is built using React, and the back end with Ruby on Rails. YelpAPI is used to search reatarants and get their information, and StripeAPI is used to process payment. It allows users to search for restaurants, add food items to their cart, checkout and view past orders.'
+                desc='Fifteen Minute Food is an app for users to order food for delivery. The front end is built using React, and the back end with Ruby on Rails. YelpAPI is used to search reatarants and get their information, and StripeAPI is used to process payment. It allows users to search for restaurants, add food items to their cart, checkout and view past orders.'
                 gitFront='https://github.com/tdonovan79/fifteen-minute-food-frontend'
                 gitBack='https://github.com/tdonovan79/fifteen_min_backend'
                 liveUrl='https://tdonovan79.github.io/fifteen-minute-food-frontend/'
@@ -31,7 +31,7 @@ export default function ProjectLinks() {
             <Project
                 name='Pokemon Battle'
                 image={Pokeball}
-                desc='Pokemon Battle is a game for users to have pokemon battles with each other. The front end is built using Vanilla JS and the back end with Ruby on Rails. The available Pokemon are populated with data from PokeAPI. It allows users to choose a team of Pokemon, choose an apponent, and enter into turn-based, two-player battles against each other.'
+                desc='Pokemon Battle is a game for users to have pokemon battles with each other. The front end is built using Vanilla JS and the back end with Ruby on Rails. The available Pokemon are populated with data from PokeAPI. It allows users to choose a team of Pokemon, choose an opponent, and enter into turn-based, two-player battles against each other.'
                 gitFront='https://github.com/tdonovan79/pokemon_battle_frontend'
                 gitBack='https://github.com/tdonovan79/pokemon-battle-backend'
                 liveUrl='https://tdonovan79.github.io/pokemon_battle_frontend/'
